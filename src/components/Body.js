cdcf5f4df8a4a25cd82660ec0d3924b3967ba4e5
maansir <m.farman4k@gmail.com>
2024-04-18 11:55:18 +0530

    basic config

---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div className="bg-cover bg-center w-full h-screen flex justify-center align-center py-10">
+  <form className='bg-black opacity-60'>
+    <input type='text' placeholder='Fistname' />
+  </form>
+    </div>
+  )
+}
+
+export default Body;
